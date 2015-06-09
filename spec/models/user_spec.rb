1df133160310ced7a80e6769ddec27a278ee8444
brayhoward <brandonrhoward@gmail.com>
2015-06-09 15:56:27 -0500

    added test around username && rails generate migration AddUsernameToUser username:string

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,9 @@ RSpec.describe User, type: :model do
     it { should have_many :posts }
     it { should validate_presence_of(:name) }
     it { should validate_length_of(:name).is_at_least(3) }
+    it { should validate_presence_of(:username) }
+    it { should validate_uniqueness_of(:username) }
+    it { should validate_length_of(:username).is_at_least(3) }
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email) }
     it { should validate_confirmation_of(:password) }
