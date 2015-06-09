94d2d8dccf79f003c289b3331827a7155b3f2a7c
kylemcco <kyle.p.mccormick@gmail.com>
2015-06-09 15:37:13 -0500

    unit test and fabricator for comment

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to :author }
+  it { should belong_to :post }
+  it { should validate_presence_of :body }
 end
