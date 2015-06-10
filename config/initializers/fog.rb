5e346c4446885b8591e473712a247acf15dd5cde
Andy Meagher <ameagher311@gmail.com>
2015-06-10 15:11:28 -0500

    installed figaro and assigned AWS keys to enviroment variables

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,12 +1,9 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',       # required
-    :aws_access_key_id      => 'xxx',       # required
-    :aws_secret_access_key  => 'yyy',       # required
-    :region                 => 'eu-west-1'  # optional, defaults to 'us-east-1'
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],       # required
+    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],       # required
+    :region                 => 'us-east-1'  # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = 'name_of_directory'                     # required
-  config.fog_host       = 'https://assets.example.com'            # optional, defaults to nil
-  config.fog_public     = false                                   # optional, defaults to true
-  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+  config.fog_directory  = 'median-development'                     # required
 end
