1965e5f8f2fa5fd5de158234e7d02ed0d8a43131
jacobdlewis <jacobdlewis@gmail.com>
2015-06-10 12:21:16 -0500

    Uploaded image is set as post header background

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,7 +3,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
@@ -24,7 +24,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process :scale => [200, 300]
+  process :resize_to_limit => [400, 300]
   #
   # def scale(width, height)
   #   # do something
