f5324e6aeb116919754547559d6c37f49826393b
Brennan Willingham <brennanw22@gmail.com>
2015-06-10 11:20:56 -0500

    Fully working happy path.

--- spec/features/list_has_20_items_spec.rb
+++ spec/features/list_has_20_items_spec.rb
@@ -1,15 +1,24 @@
 feature "List only has twenty items per page" do
   # The search result pages only shoe 20 entries per page.
   describe "limited showing of posts" do
+
     before(:each) do
       # PostsController.set_instance_variable(@per, 20)
     end
 
-    scenario "posts list" do
-      me = Fabricate(:user, name: "Jim-Bob")
+    scenario "posts list happy path" do
+      me = Fabricate(:user)
+      30.times do
+        Fabricate(:post, author: me)
+      end
       signin_as me
-      visit index_posts_path
-      page.should have_css('.pagination')
+      render_template layout: "users/index"
+      visit user_posts_path(me)
+      page.should have_css('ul.pagination')
+      within("ul.pagination") do
+        page.should have_css("li:nth-child(1)")
+        page.should_not have_css("li:nth-child(21)")
+      end
     end
   end
 end
