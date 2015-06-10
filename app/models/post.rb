b00b0f4b1ba8678ee5133f849dfd0123b85c5903
jacobdlewis <jacobdlewis@gmail.com>
2015-06-10 09:51:37 -0500

    local image uploads work

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
+  attr_accessor :image
+  mount_uploader :image, ImageUploader
   belongs_to :author, class_name: "User"
-
   validates :author, :body, :title, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150609162545) do
+ActiveRecord::Schema.define(version: 20150609200725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150609162545) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
   add_index "posts", ["author_id"], name: "index_posts_on_author_id", using: :btree
