5267b4e31421876f477397bff0744ed3314b59bd
brayhoward <brandonrhoward@gmail.com>
2015-06-09 19:44:18 -0500

    edited user model and  user controller and ran [rake db:migrate]

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,10 @@ class User < ActiveRecord::Base
 
   has_many :posts, foreign_key: :author_id
 
-  validates :email, :name, presence: true
-  validates :name, length: { minimum: 3 }
+  validates :email, :name, :username, presence: true
+  validates :name, :username, length: { minimum: 3 }
   validates :email, format: { with: /.+@.+\..+/, message: "must be an email address" }, uniqueness: true
+  validates :username, uniqueness: true
   validates :password, confirmation: true
   validates :password, :password_confirmation, presence: { on: :create }
 end
