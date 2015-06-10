e0f3758a0885cbe319d3682f9ed9a14d6c767232
brayhoward <brandonrhoward@gmail.com>
2015-06-10 15:48:23 -0500

    added character limit of (3) to username in user Fabricator

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -1,7 +1,7 @@
 Fabricator(:user) do
   name   { Faker::Name.name }
   email  { Faker::Internet.email }
-  username { Faker::Internet.user_name }
+  username { Faker::Internet.user_name(3) }
   password              "password1"
   password_confirmation "password1"
 end
