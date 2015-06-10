86d15376a1701b30e58e612ed0e1e43b99e0c10c
kylemcco <kyle.p.mccormick@gmail.com>
2015-06-10 11:05:27 -0500

    work on integration test user_creates_comment

--- spec/features/user_creates_comment_spec.rb
+++ spec/features/user_creates_comment_spec.rb
@@ -1,4 +1,16 @@
 feature "user creates comment" do
+
+  let(:julie){ Fabricate(:user, name: "Julie") }
+
+  background do
+    bob = Fabricate(:user, name: "Bob")
+    ingrid = Fabricate(:user, name: "Ingrid")
+    Fabricate(:post, author: bob, title: "Bob's Burger Recipe")
+    Fabricate(:post, author: julie, title: "Julie's Intro to XPath", body: "XPath is *great*")
+    Fabricate(:post, author: julie, title: "Julie's Over XPath", body: "XPath is *great*")
+    visit root_path
+  end
+
   # As a comment-poster,
   # In order to share my comments to the post,
   # I want to publish a comment
@@ -6,38 +18,38 @@ feature "user creates comment" do
   # * Comment must have a body
   # * Comment must be publicly visible once saved
 
-  scenario "logged out users can't create comments" do
-    visit root_path
-    page.should_not have_content("Share Some Knowledge")
-    visit new_post_path
-    should_be_denied_access
-  end
+  # scenario "logged out users can't create comments" do
+  #   visit root_path
+  #   page.should_not have_content("Share Some Knowledge")
+  #   visit new_post_path
+  #   should_be_denied_access
+  # end
 
   scenario "happy path" do
     me = Fabricate(:user, name: "Bob")
-    entry = Fabricate(:post, title: "Tom's Post")
-    author = entry.author
     signin_as me
-    click_on "#{@author}'s Knowledge"
-    page.should have_content("#{@author}'s Post")
-    click_on "Tom's Post"
-    current_path.should == user_posts_path(entry)
+    click_on "Bob's Knowledge"
+    page.should have_content("Bob's Burger Recipe")
+    click_on "Bob's Burger Recipe"
+    page.should have_css("h1", text: "Bob's Burger Recipe")
+    save_and_open_page
     fill_in "Body", with: "Here is my comment to you, Tom."
-    click_on "Submit Comment"
+    click_on "Publish Comment"
     page.should have_notice("Your comment has been published")
     current_path.should == user_posts_path(entry)
     page.should have_css(".comment", text: "Here's my comment to you, Tom.")
    end
 
-  scenario "sad path" do
-    me = Fabricate(:user, name: "Bob")
-    signin_as me
-    click_on "Share Some Knowledge"
-    fill_in "Title", with: ""
-    fill_in "Body", with: ""
-    click_on "Publish Knowledge"
-    page.should have_alert("Your knowledge could not be published. Please correct the errors below.")
-    page.should have_error("can't be blank", on: "Title")
-    page.should have_error("can't be blank", on: "Body")
-  end
+  # scenario "sad path" do
+  #   pending
+  #   me = Fabricate(:user, name: "Bob")
+  #   signin_as me
+  #   click_on "Share Some Knowledge"
+  #   fill_in "Title", with: ""
+  #   fill_in "Body", with: ""
+  #   click_on "Publish Knowledge"
+  #   page.should have_alert("Your knowledge could not be published. Please correct the errors below.")
+  #   page.should have_error("can't be blank", on: "Title")
+  #   page.should have_error("can't be blank", on: "Body")
+  # end
 end
