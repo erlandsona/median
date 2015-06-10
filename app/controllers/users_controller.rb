670c094227bafe7274932433fb94a88af00ae620
Luke Lancaster <luketlancaster@gmail.com>
2015-06-10 10:12:44 -0500

    Implement edit feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,9 +22,25 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.assign_attributes(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+      flash.notice = "Your profile has been updated"
+    else
+      flash.alert = "Please fix the errors below to continue"
+      render :edit
+    end
+  end
+
   protected
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :bio, :password, :password_confirmation)
   end
 end
