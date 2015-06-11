fece70117437cb6cdd6680706b6206be29dd82d2
jacobdlewis <jacobdlewis@gmail.com>
2015-06-11 09:33:07 -0500

    WIP testing uploads to S3

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,9 +1,16 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',       # required
-    :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],       # required
-    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],       # required
-    :region                 => 'us-east-1'  # optional, defaults to 'us-east-1'
-  }
-  config.fog_directory  = 'median-development'                     # required
+if Rails.env.test?
+  CarrierWave.configure do |config|
+    config.storage = :file
+    config.enable_processing = false
+  end
+# else
+#   CarrierWave.configure do |config|
+#     config.fog_credentials = {
+#       :provider               => 'AWS',       # required
+#       :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],       # required
+#       :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],       # required
+#       :region                 => 'us-east-1'  # optional, defaults to 'us-east-1'
+#     }
+#     config.fog_directory  = 'median-development'                     # required
+#   end
 end
