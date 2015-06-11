b25048c131b43e3a018de9cf30c09ab37cfe77c0
brayhoward <brandonrhoward@gmail.com>
2015-06-11 14:31:26 -0500

    removed show from users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,6 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def show
-    @username = User.find(params[:id])
-  end
-
-
   def create
     @user = User.new(user_params)
     if @user.save
