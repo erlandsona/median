3710fee5ff5ede70db359e5f6fc96bb6835de144
Katye Russell <katye.russell@gmail.com>
2015-06-12 11:12:11 -0500

    Changed published_at to nil (instead of a later date).

--- spec/fabricators/draft_fabricator.rb
+++ spec/fabricators/draft_fabricator.rb
@@ -2,5 +2,5 @@ Fabricator(:draft, from: :post) do
   author(fabricator: :user)
   title { Faker::Lorem.words(4) }
   body { Faker::Lorem.paragraph(2) }
-  published_at { Faker::Date.forward(5) }
+  published_at { nil }
 end
